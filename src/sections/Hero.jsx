8a8b6feeca3351342a1b33bc290680b61716858e
solersol <ttpage1tk@gmail.com>
2024-08-31 03:04:32 +0300

    vfd

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -3,9 +3,9 @@ const Hero = () => {
     <section id="home" className="w-full h-screen flex flex-col justify-center items-center min-h-screen pt-[10rem] bg-cover bg-center">
       {/* <h1 className="flex justify-center font-comic  text-[70px] mb-0 text-[#fff]">$F1</h1> */}
       {/* <h2 className="flex justify-center font-comic  text-[22px] mb-10 mt-0 text-[#dfe2ff]">F1 MEMES</h2> */}
-      <h1 className="flex justify-center font-elite  text-[35px] mt-6 text-center px-4 break-words font-black text-[#fff] animate-slide-in-left">CA:   </h1>
+      <h1 className="flex justify-center font-elite  text-[35px] mt-6 text-center px-4 break-words font-black text-[#fff] animate-slide-in-left">CA: HAUXmmKs8Pe2bjNjH1jxcuVM5FHkzLLgi6EUz31kpump  </h1>
 
-      <a href="https://pump.fun/"><button className="animate-pulse mt-6 bg-[#81061d] text-[#000] px-28 py-6 hover:bg-[#36160f] border-4 border-black font-black  font-elite  text-[24px] ">
+      <a href="https://pump.fun/HAUXmmKs8Pe2bjNjH1jxcuVM5FHkzLLgi6EUz31kpump"><button className="animate-pulse mt-6 bg-[#81061d] text-[#000] px-28 py-6 hover:bg-[#36160f] border-4 border-black font-black  font-elite  text-[24px] ">
         BUY $AVTU
       </button></a>
 
--- src/sections/StickyFooter.jsx
+++ src/sections/StickyFooter.jsx
@@ -10,7 +10,7 @@ const StickyFooter = () => {
         <a href="https://x.com/AverageTronUser">
           <img src={img1} alt="Image 1" className="w-16 h-16 sm:w-12 sm:h-12 transition-transform transform hover:scale-125" />
         </a>
-        <a href="https://pump.fun/">
+        <a href="https://pump.fun/HAUXmmKs8Pe2bjNjH1jxcuVM5FHkzLLgi6EUz31kpump">
           <img src={img2} alt="Image 2" className="w-16 h-16 sm:w-12 sm:h-12 transition-transform transform hover:scale-125" />
         </a>
         <a href="https://t.me/AverageTronHolder">
